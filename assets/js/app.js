a264fc23287da18bff9e13ecd7b032dca05051ba
Rayli Raykov <rayli208@gmail.com>
2019-01-14 21:16:33 -0500

    added the dragabble addon and also added fixed background on load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,42 @@
 //Make cards
-$(document).ready(function(){
-  var giantContainer = localStorage.getItem('giantContainer');
-  var counter = localStorage.getItem('counter') || 0;
+$(document).ready(function() {
+  var giantContainer = localStorage.getItem("giantContainer");
+  var counter = localStorage.getItem("counter") || 0;
 
-  if(giantContainer){
+  if (giantContainer) {
     $("#giantContainer").html(giantContainer);
   }
-  
-  $("#saveUserInfo").click(function (e) {
+
+  $("#saveUserInfo").click(function(e) {
     e.preventDefault();
-    var company = $("#company").val().trim();
-    var jobTitle = $("#title").val().trim();
-    var phoneNumber = $("#phone").val().trim();
-    var email = $("#email").val().trim();
-    var location = $("#location").val().trim();
-    var link = $("#link").val().trim();
-    var salary = $("#salary").val().trim();
-    var info = $("#text-area").val().trim();
-  
-    var card = `<div class="card mx-auto mb-3" style="width: 18rem;">
-    <div class="card">
+    var company = $("#company")
+      .val()
+      .trim();
+    var jobTitle = $("#title")
+      .val()
+      .trim();
+    var phoneNumber = $("#phone")
+      .val()
+      .trim();
+    var email = $("#email")
+      .val()
+      .trim();
+    var location = $("#location")
+      .val()
+      .trim();
+    var link = $("#link")
+      .val()
+      .trim();
+    var salary = $("#salary")
+      .val()
+      .trim();
+    var info = $("#text-area")
+      .val()
+      .trim();
+    var id = "card-" + counter;
+
+    var card = `<div id="${id}" class="card mx-auto mb-3 mt-2" style="width: 18rem;">
+    <div class="card-top">
       <button class="btn card-header bg-purple" type="button" data-toggle="collapse" data-target="#cardCollapse${counter}"
         aria-expanded="false" aria-controls="cardCollapse${counter}">
         <h5 class="card-title">${company}</h5>
@@ -49,88 +66,128 @@ $(document).ready(function(){
       <i class="fas fa-trash-alt trash"></i>
     </div>
   </div>`;
-  
+
     var applied = $("#applied");
     applied.append(card);
 
-    $("#company").val('');
-    $("#title").val('');
-    $("#phone").val('');
-    $("#email").val('');
-    $("#location").val('');
-    $("#link").val('');
-    $("#salary").val('');
-    $("#text-area").val('');
-    
-    counter++;
+    $("#" + id)
+      .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+      .find(".card-header")
+      .addClass("ui-widget-header ui-corner-all");
 
-    var giantContainer = $("#giantContainer").html();
-    localStorage.setItem('giantContainer', giantContainer);
-    localStorage.setItem('counter', counter);
+    $("#company").val("");
+    $("#title").val("");
+    $("#phone").val("");
+    $("#email").val("");
+    $("#location").val("");
+    $("#link").val("");
+    $("#salary").val("");
+    $("#text-area").val("");
+
+    counter++;
+    persist();
   });
-  
+
+  function persist() {
+    var giantContainer = $("#giantContainer").html();
+    localStorage.setItem("giantContainer", giantContainer);
+    localStorage.setItem("counter", counter);
+  }
+
   $(document).on("click", ".trash", function(e) {
     e.preventDefault();
     console.log(this);
-    if (confirm('You sure wanna delete this job?')) {
-      ($(this).parent().parent()[0].remove());
+    if (confirm("You sure wanna delete this job?")) {
+      $(this)
+        .parent()
+        .parent()[0]
+        .remove();
     } else {
       false;
     }
     var giantContainer = $("#giantContainer").html();
-    localStorage.setItem('giantContainer', giantContainer);
+    localStorage.setItem("giantContainer", giantContainer);
   });
-})
 
-
-
+  $(function() {
+    $(".jobList").sortable({
+      connectWith: ".jobList",
+      handle: ".card-title",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all",
+      stop: function(event, ui) {
+        persist();
+      }
+    });
+  });
+});
 
 //API Search
-$("#search-user-input").click(function (e) {
+$("#search-user-input").click(function(e) {
   e.preventDefault();
   $("#box").empty();
 
- 
-//  var appID = 'd8d73b54';
-//  var appKey = 'f48deeda8d68ea1d2e670db1346ab43f';
- var userSearch = $("#user-search-job").val().trim();
- var userCity = $("#user-search-city").val().trim();
- 
- var queryURL = `http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d8d73b54&app_key=f48deeda8d68ea1d2e670db1346ab43f&results_per_page=10&what=${userSearch}&where=${userCity}&content-type=application/json`;
- 
- $.ajax({
-  url: queryURL,
-  method: "GET"
- }).then(function (response) {
-  // console.log(response.results.length)
- 
-  // console.log(response.results[0])
- 
-  for (var i = 0; i < response.results.length; i++) {
- 
-    var title = response.results[i].title
-    var company = response.results[i].company.display_name
-    var location = response.results[i].location.display_name
-    var jobLink = response.results[i].redirect_url
- 
-    var card =
-    `<div class="card text-center mb-3">
-   <div class="card-header bg-purple text-white">
+  //  var appID = 'd8d73b54';
+  //  var appKey = 'f48deeda8d68ea1d2e670db1346ab43f';
+  var userSearch = $("#user-search-job")
+    .val()
+    .trim();
+  var userCity = $("#user-search-city")
+    .val()
+    .trim();
+
+  var queryURL = `http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d8d73b54&app_key=f48deeda8d68ea1d2e670db1346ab43f&results_per_page=10&what=${userSearch}&where=${userCity}&content-type=application/json`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    // console.log(response.results.length)
+
+    // console.log(response.results[0])
+
+    for (var i = 0; i < response.results.length; i++) {
+      var title = response.results[i].title;
+      var company = response.results[i].company.display_name;
+      var location = response.results[i].location.display_name;
+      var jobLink = response.results[i].redirect_url;
+
+      var card = `<div class="card card-border text-center mb-3">
+   <div class="card-header card-header-purple bg-pink text-white">
      <strong><span id="user-search-title">${title}</span></strong>
    </div>
    <div class="card-body">
      <p class="card-text"><strong>Company</strong>: <span id="user-search-company">${company}</span></p>
      <p class="card-text"><strong>Location</strong>: <span id="user-search-location">${location}</span></p>
    </div>
-   <div class="card-footer text-white bg-pink">
+   <div class="card-footer card-footer-purple text-white bg-pink">
      <a target="_blank" href="${jobLink}" class="btn btn-block btn-light">Apply</a>
    </div>
- </div>`
- 
- $("#box").append(card);
- 
-  }
- 
- });
- 
+ </div>`;
+
+      $("#box").append(card);
+    }
   });
+});
+
+(function() {
+  function resize() {
+    var height = $(window).height();
+    height = height - $(".navbar")[0].offsetHeight;
+    height = height - $(".jumbotron")[0].offsetHeight;
+    $("#giantContainer").attr("style", "min-height:" + height + "px;");
+    height =
+      height -
+      $("#giantContainer")
+        .find(".row")
+        .first()[0].offsetHeight;
+    $("#applied").attr("style", "min-height:" + height + "px;");
+  }
+
+  $(document).ready(function() {
+    resize();
+  });
+  $(window).resize(function() {
+    resize();
+  });
+});
